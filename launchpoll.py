f5ae9f68f1ed67a524e6f7f38ee7230915b0176d
Viljo Wilding <viljo.wilding@uwe.ac.uk>
2024-02-19 13:56:26 +0000

    add debug flag to automatically open the right election

--- launchpoll.py
+++ launchpoll.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 args = sys.argv[1:]
-if not args[-1]:
+if len(args)==0:
     ID="15999999" # test ID
 else:
     ID=args[-1]
@@ -19,18 +19,19 @@ with sync_playwright() as p:
          browser = p.chromium.launch(channel="msedge",headless=False,args=["--kiosk"])
          page = browser.new_context().new_page()
     page.goto("https://uwe.admin.ukmsl.net/voting/")
-    if page.title() == "The Students' Union at UWE | Log In":
-        page.get_by_label("Username").fill(os.getenv('MSL_USERNAME'))
-        page.get_by_label("Password").fill(os.getenv('MSL_PASSWORD'))
-        page.get_by_text("Log in").click()
-        page.wait_for_load_state()
+    page.get_by_label("Username").fill(os.getenv('MSL_USERNAME'))
+    page.get_by_label("Password").fill(os.getenv('MSL_PASSWORD'))
+    page.get_by_text("Log in").click()
+    page.wait_for_load_state()
     page.get_by_role("button", name="Continue").click()
     page.get_by_role("textbox").fill(ID)
     page.get_by_text("Go").click()
-    # up to here, it works. need to handle the quitting aspect... maybe wait until back to the logged out page then close the browser?
-    print(page.url)
     print("selecting election...")
-    page.wait_for_url("**/SelectPost.aspx",timeout=None)
+    if not os.getenv('DEBUG'):
+        # viljo remember to change this
+        page.locator("input#ContentPlaceHolder_rptElections_btnSelect_2").click()
+    else:
+        page.wait_for_url("**/SelectPost.aspx",timeout=None)
     print("selecting post...")
     page.wait_for_url("**/SelectElection.aspx",timeout=None)
     print("back to select election!")
