e520b7c68186288a9b569851d224b8bdb83dbdb5
Viljo Wilding <viljo.wilding@uwe.ac.uk>
2024-03-03 14:32:54 +0000

    add setup file

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @set path=%path%;%PUBLIC%;%ProgramFiles%\Google\Chrome\Application
-@pip3 install python-dotenv playwright requests psutil
 @pythonw daemon.py
 @pythonw client-script.py
 @pythonw daemon.py offline
